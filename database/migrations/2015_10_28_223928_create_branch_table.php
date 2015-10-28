c84e79f577bf10dae8a364b65752a577368430c4
Antonio1394 <antoniorivm1394@gmail.com>
2015-10-28 16:43:40 -0600

    [add] first migrations

---
+++ database/migrations/2015_10_28_223928_create_branch_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branch', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idHost');
+            $table->foreign('idHost')->references('id')->on('host');
+            $table->string('nombre');
+            $table->string('area');
+            $table->date('fecha');
+            $table->string('hora');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('branch');
+    }
+}
