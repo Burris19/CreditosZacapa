b81eb81b1d5f4f0b1e6feeeaae365184528a9116
Antonio1394 <antoniorivm1394@gmail.com>
2015-10-29 15:28:06 -0600

    [add] Migration host Branch

---
+++ database/migrations/2015_10_28_222629_create_empleados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idUsuario');
+            $table->foreign('idUsuario')->referenes('id')->on('users');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('direccion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('empleados');
+    }
+}
